397a4c4560a4a7c61c6b8c8d74e6e42547b110e4
Jeremy Letang <letang.jeremy@gmail.com>
2013-06-04 11:40:55 -0700

    remove unused trait

--- src/rsfml.rs
+++ src/rsfml.rs
@@ -6,9 +6,3 @@ pub mod sfTypes {
     pub static sfFalse : sfBool = 0;
     pub static sfTrue : sfBool = 1;
 }
-
-#[doc(hidden)]
-pub trait Wrapper {
-    pub fn wrap_color<T>(sfObj : T) -> Self;
-    pub fn unwrap_color(&self) -> Self; 
-}
