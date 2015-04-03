4de32825013e097ac830e8e67d50c54a8ec03162
Mika Attila <radiantstatue@gmail.com>
2015-04-03 09:18:32 +0200

    Depend on crates.io libc instead of unstable bundled libc

--- src/lib.rs
+++ src/lib.rs
@@ -125,7 +125,7 @@
 #![crate_type = "dylib"]
 #![allow(dead_code, non_camel_case_types, unused_attributes)]
 #![warn(missing_docs)]
-#![feature(libc, core)]
+#![feature(core)]
 
 extern crate libc;
 extern crate core;
