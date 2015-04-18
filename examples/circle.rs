a945ae859ca9c1ca87eccba697d27dd63ea9b3b0
Mika Attila <radiantstatue@gmail.com>
2015-04-18 10:13:46 +0200

    Update circle example in README.md, add it to examples/ for easy testing

--- examples/circle.rs
+++ examples/circle.rs
@@ -1,40 +1,3 @@
-rust-sfml [![Build Status](https://api.travis-ci.org/jeremyletang/rust-sfml.png?branch=master)](https://travis-ci.org/jeremyletang/rust-sfml)
-=========
-
-
-SFML bindings for Rust
-
-This is a Rust binding for SFML, the Simple and Fast Multimedia Library, developed by Laurent Gomila.
-
-SFML website : www.sfml-dev.org
-
-Installation
-============
-
-You must install the SFML2.1 and CSFML2.1 libraries on your computer which are used for the binding.
-
-SFML2.1: http://www.sfml-dev.org/download/sfml/2.1/
-
-CSFML2.1: http://www.sfml-dev.org/download/csfml/
-
-Then clone the repo and build the library with the following command.
-
-You can use Cargo to build rust-sfml:
-```Shell
-> cargo build
-```
-
-Examples are located under the `examples` directory.
-You can run an example with `cargo run --example example_name`
-
-Rust-sfml works on Linux, Windows and OSX.
-
-Short example
-=============
-
-Here is a short example, draw a circle shape and display it.
-
-```Rust
 extern crate sfml;
 
 use sfml::system::Vector2f;
@@ -72,14 +35,3 @@ fn main() {
         window.display()
     }
 }
-
-```
-
-
-License
-=======
-
-This software is a binding of the SFML library created by Laurent Gomila, which is provided under the Zlib/png license.
-
-This software is provided under the same license than the SFML, the Zlib/png license.
-
