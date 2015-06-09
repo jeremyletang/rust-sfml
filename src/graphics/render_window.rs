a14ee56c9d9f6d49f05abeda3a4241406fdebb4f
Mika Attila <radiantstatue@gmail.com>
2015-06-09 10:51:02 +0200

    Remove a redundant .as_bytes() call in RenderWindow::new()

--- src/graphics/render_window.rs
+++ src/graphics/render_window.rs
@@ -85,7 +85,7 @@ impl RenderWindow {
                title: &str,
                style: WindowStyle,
                settings: &ContextSettings) -> Option<RenderWindow> {
-        let c_str = CString::new(title.as_bytes()).unwrap();
+        let c_str = CString::new(title).unwrap();
         let sf_render_win: *mut ffi::sfRenderWindow = unsafe {
             ffi::sfRenderWindow_create(mode.unwrap(),
                                        c_str.as_ptr(),
