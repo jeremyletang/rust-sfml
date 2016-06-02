cd57b4e85aca258149615c1812477b345933b876
Mika Attila <radiantstatue@gmail.com>
2016-06-02 09:49:31 +0200

    Have custom sound stream example fade out and stop after a while

--- examples/custom-sound-stream.rs
+++ examples/custom-sound-stream.rs
@@ -1,12 +1,13 @@
 extern crate sfml;
 
-use sfml::audio::{SoundStream, SoundStreamPlayer};
+use sfml::audio::{SoundStatus, SoundStream, SoundStreamPlayer};
 use sfml::system::Time;
 
 // Melody by ryg - https://youtu.be/tCRPUv8V22o?t=176
 struct BitMelody {
     buf: [i16; 2048],
     t: i32,
+    amp: i16,
 }
 
 impl SoundStream for BitMelody {
@@ -19,9 +20,13 @@ impl SoundStream for BitMelody {
             let note = t * (melody[index as usize] as i32 & 15);
             let sample = (note / 12 & 128) +
                          (((((t >> 12) ^ (t >> 12) - 2) % 11 * t) / 4 | t >> 13) & 127);
-            *buf_sample = sample as i16 * 128;
+            *buf_sample = sample as i16 * self.amp;
+            // Fade out after a while
+            if t > 1_048_576 && t % 4096 == 0 {
+                self.amp -= 1;
+            }
         }
-        (&mut self.buf[..], true)
+        (&mut self.buf[..], self.amp > 0)
     }
     fn seek(&mut self, offset: Time) {
         // Not exactly correct, but meh.
@@ -40,6 +45,7 @@ impl BitMelody {
         BitMelody {
             buf: [0; 2048],
             t: 0,
+            amp: 128,
         }
     }
 }
@@ -48,7 +54,7 @@ fn main() {
     let mut stream = BitMelody::new();
     let mut player = SoundStreamPlayer::new(&mut stream);
     player.play();
-    loop {
+    while player.get_status() == SoundStatus::Playing {
         ::std::thread::sleep(::std::time::Duration::from_millis(100));
     }
 }
