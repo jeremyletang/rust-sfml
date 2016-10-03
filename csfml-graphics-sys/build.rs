22622cb84ff7fe913922c8275b7f054e02c314c4
Jeffrey Massung <massung@gmail.com>
2016-10-03 09:53:20 -0500

    Using target_env and CSFML_HOME in build scripts.
    Uses cfg!(target_env) in system build scripts to get the name
    of the library to link correct:
    
    target_env(msvc) -> csfml-*
    target_env(gnu) -> libcsfml-*
    
    Also added an option CSFML_HOME environment variable check so
    the libraries can be located somewhere other than the root
    directory of the project using SFML.

--- csfml-graphics-sys/build.rs
+++ csfml-graphics-sys/build.rs
@@ -1,3 +1,24 @@
+use std::env::var;
+
+fn lib_path() -> String {
+    let lib = if cfg!(target_env = "msvc") {
+        "csfml-graphics"
+    } else {
+        "libcsfml-graphics"
+    };
+
+    // check if CSFML is installed somewhere local on the machine
+    if let Ok(csfml_home) = var("CSFML_HOME") {
+        if cfg!(target_env = "msvc") {
+            format!("{}/lib/msvc/{}", csfml_home, lib)
+        } else {
+            format!("{}/lib/gcc/{}", csfml_home, lib)
+        }
+    } else {
+        String::from(lib)
+    }
+}
+
 fn main() {
-    println!("cargo:rustc-link-lib=csfml-graphics");
+    println!("cargo:rustc-link-lib={}", lib_path());
 }
