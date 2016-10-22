5e2ada90a2773936d89c938ef0d3565ffb03884d
Mika Attila <radiantstatue@gmail.com>
2016-10-22 11:07:56 +0200

    Update documentation for Time

--- src/system/time.rs
+++ src/system/time.rs
@@ -37,6 +37,34 @@ use csfml_system_sys as ffi;
 /// Represents a time value.
 ///
 /// Time encapsulates a time value in a flexible way.
+///
+/// It allows to define a time value either as a number of seconds, milliseconds or microseconds.
+/// It also works the other way round: you can read a time value as either a number of seconds,
+/// milliseconds or microseconds.
+///
+/// By using such a flexible interface, the API doesn't impose any fixed type or resolution for
+/// time values, and let the user choose its own favorite representation.
+///
+/// Time values support the usual mathematical operations: you can add or subtract two times,
+/// multiply or divide a time by a number, compare two times, etc.
+///
+/// Since they represent a time span and not an absolute time value, times can also be negative.
+///
+/// # Usage example
+/// ```
+/// # use sfml::system::Time;
+/// let t1 = Time::with_seconds(0.1);
+/// assert_eq!(t1.as_milliseconds(), 100);
+///
+/// let t2 = Time::with_milliseconds(30);
+/// assert_eq!(t2.as_microseconds(), 30_000);
+///
+/// let t3 = Time::with_microseconds(-800_000);
+/// assert_eq!(t3.as_seconds(), -0.8);
+/// ```
+///
+/// # See also
+/// - `Clock`
 #[derive(Copy, Clone)]
 pub struct Time {
     time: ffi::sfTime,
