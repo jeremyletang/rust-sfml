8a1413fa83b8c91a8a6baaa6b9379a0ae59eb54b
Mika Attila <radiantstatue@gmail.com>
2016-10-24 23:14:16 +0200

    Make system::Clock a newtype wrapper

--- src/system/clock.rs
+++ src/system/clock.rs
@@ -45,14 +45,12 @@ use csfml_system_sys as ffi;
 ///
 /// The `Time` value returned by the clock can then be converted to
 /// a number of seconds, milliseconds or even microseconds.
-pub struct Clock {
-    clock: *mut ffi::sfClock,
-}
+pub struct Clock(*mut ffi::sfClock);
 
 impl Clock {
     /// Creates a new Clock and starts it automatically.
     pub fn start() -> Clock {
-        Clock { clock: unsafe { ffi::sfClock_create() } }
+        Clock(unsafe { ffi::sfClock_create() })
     }
 
     /// Gets the elapsed time.
@@ -60,7 +58,7 @@ impl Clock {
     /// This function returns the time elapsed since the last call to restart()
     /// (or the construction of the instance if restart() has not been called).
     pub fn elapsed_time(&self) -> Time {
-        unsafe { Time::from_raw(ffi::sfClock_getElapsedTime(self.clock)) }
+        unsafe { Time::from_raw(ffi::sfClock_getElapsedTime(self.0)) }
     }
 
     /// Restarts the clock.
@@ -68,19 +66,19 @@ impl Clock {
     /// This function puts the time counter back to zero.
     /// It also returns the time elapsed since the clock was started.
     pub fn restart(&mut self) -> Time {
-        unsafe { Time::from_raw(ffi::sfClock_restart(self.clock)) }
+        unsafe { Time::from_raw(ffi::sfClock_restart(self.0)) }
     }
 }
 
 impl Clone for Clock {
-    fn clone(&self) -> Clock {
-        Clock { clock: unsafe { ffi::sfClock_copy(self.clock) } }
+    fn clone(&self) -> Self {
+        Clock(unsafe { ffi::sfClock_copy(self.0) })
     }
 }
 
 impl Drop for Clock {
     fn drop(&mut self) {
-        unsafe { ffi::sfClock_destroy(self.clock) }
+        unsafe { ffi::sfClock_destroy(self.0) }
     }
 }
 
