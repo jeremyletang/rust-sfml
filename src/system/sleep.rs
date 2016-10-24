80ee644668352909865f7b6dbfc99e0eba117230
Mika Attila <radiantstatue@gmail.com>
2016-10-24 20:59:54 +0200

    Punctuation

--- src/system/sleep.rs
+++ src/system/sleep.rs
@@ -24,7 +24,7 @@
 use raw_conv::Raw;
 use system::Time;
 
-/// Make the current thread sleep for a given duration
+/// Make the current thread sleep for a given duration.
 pub fn sleep(time: Time) {
     unsafe { ::csfml_system_sys::sfSleep(time.raw()) }
 }
