7096e7b3598963e7f9a974d53d09c57baf5ccadc
Mika Attila <radiantstatue@gmail.com>
2016-10-26 21:44:44 +0200

    Add SfBool type

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -31,9 +31,11 @@ pub use self::vector3::{Vector3, Vector3i, Vector3f};
 pub use self::sleep::sleep;
 pub use self::time::{Time, ZERO as TIME_ZERO};
 pub use self::clock::Clock;
+pub use self::sf_bool::{SfBool, TRUE as SF_TRUE, FALSE as SF_FALSE};
 
 mod time;
 mod clock;
 mod sleep;
 mod vector2;
 mod vector3;
+mod sf_bool;
