b5bce3562786740288fff2082111740db71d8d98
Mika Attila <radiantstatue@gmail.com>
2017-09-05 20:27:27 +0200

    Move BlendMode constants to associated consts on BlendMode

--- src/graphics/blend_mode.rs
+++ src/graphics/blend_mode.rs
@@ -64,49 +64,9 @@ pub struct BlendMode {
 impl Default for BlendMode {
     /// Default blending mode is alpha blending.
     fn default() -> Self {
-        ALPHA
+        Self::ALPHA
     }
 }
-
-/// "Alpha" blend mode
-pub const ALPHA: BlendMode = BlendMode {
-    color_src_factor: Factor::SrcAlpha,
-    color_dst_factor: Factor::OneMinusSrcAlpha,
-    color_equation: Equation::Add,
-    alpha_src_factor: Factor::One,
-    alpha_dst_factor: Factor::OneMinusSrcAlpha,
-    alpha_equation: Equation::Add,
-};
-
-/// "Add" blend mode
-pub const ADD: BlendMode = BlendMode {
-    color_src_factor: Factor::SrcAlpha,
-    color_dst_factor: Factor::One,
-    color_equation: Equation::Add,
-    alpha_src_factor: Factor::One,
-    alpha_dst_factor: Factor::One,
-    alpha_equation: Equation::Add,
-};
-
-/// "Multiply" blend mode
-pub const MULTIPLY: BlendMode = BlendMode {
-    color_src_factor: Factor::DstColor,
-    color_dst_factor: Factor::Zero,
-    color_equation: Equation::Add,
-    alpha_src_factor: Factor::DstColor,
-    alpha_dst_factor: Factor::Zero,
-    alpha_equation: Equation::Add,
-};
-
-/// "None" blend mode
-pub const NONE: BlendMode = BlendMode {
-    color_src_factor: Factor::One,
-    color_dst_factor: Factor::Zero,
-    color_equation: Equation::Add,
-    alpha_src_factor: Factor::One,
-    alpha_dst_factor: Factor::Zero,
-    alpha_equation: Equation::Add,
-};
 
 /// Enumeration of the blending factors.
 ///
@@ -174,4 +134,43 @@ impl BlendMode {
     pub(super) fn raw(&self) -> ffi::sfBlendMode {
         unsafe { ::std::mem::transmute(*self) }
     }
+    /// "Alpha" blend mode
+    pub const ALPHA: BlendMode = BlendMode {
+        color_src_factor: Factor::SrcAlpha,
+        color_dst_factor: Factor::OneMinusSrcAlpha,
+        color_equation: Equation::Add,
+        alpha_src_factor: Factor::One,
+        alpha_dst_factor: Factor::OneMinusSrcAlpha,
+        alpha_equation: Equation::Add,
+    };
+
+    /// "Add" blend mode
+    pub const ADD: BlendMode = BlendMode {
+        color_src_factor: Factor::SrcAlpha,
+        color_dst_factor: Factor::One,
+        color_equation: Equation::Add,
+        alpha_src_factor: Factor::One,
+        alpha_dst_factor: Factor::One,
+        alpha_equation: Equation::Add,
+    };
+
+    /// "Multiply" blend mode
+    pub const MULTIPLY: BlendMode = BlendMode {
+        color_src_factor: Factor::DstColor,
+        color_dst_factor: Factor::Zero,
+        color_equation: Equation::Add,
+        alpha_src_factor: Factor::DstColor,
+        alpha_dst_factor: Factor::Zero,
+        alpha_equation: Equation::Add,
+    };
+
+    /// "None" blend mode
+    pub const NONE: BlendMode = BlendMode {
+        color_src_factor: Factor::One,
+        color_dst_factor: Factor::Zero,
+        color_equation: Equation::Add,
+        alpha_src_factor: Factor::One,
+        alpha_dst_factor: Factor::Zero,
+        alpha_equation: Equation::Add,
+    };
 }
