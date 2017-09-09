63e084aecdd2c879c6512c71660ef259cf5ab0cc
Mika Attila <radiantstatue@gmail.com>
2017-09-09 09:59:20 +0200

    Remove some unused items from csfml-audio-sys

--- ffi/csfml-audio-sys/src/lib.rs
+++ ffi/csfml-audio-sys/src/lib.rs
@@ -63,34 +63,6 @@ pub struct sfSoundRecorder {
 pub struct sfSoundStream {
     _unused: [u8; 0],
 }
-pub type wchar_t = ::std::os::raw::c_int;
-#[repr(C)]
-#[derive(Debug, Copy)]
-pub struct max_align_t {
-    pub __clang_max_align_nonce1: ::std::os::raw::c_longlong,
-    pub __bindgen_padding_0: u64,
-    pub __clang_max_align_nonce2: f64,
-}
-#[test]
-fn bindgen_test_layout_max_align_t() {
-    assert_eq!(::std::mem::size_of::<max_align_t>() , 32usize , concat ! (
-               "Size of: " , stringify ! ( max_align_t ) ));
-    assert_eq! (unsafe {
-                & ( * ( 0 as * const max_align_t ) ) .
-                __clang_max_align_nonce1 as * const _ as usize } , 0usize ,
-                concat ! (
-                "Alignment of field: " , stringify ! ( max_align_t ) , "::" ,
-                stringify ! ( __clang_max_align_nonce1 ) ));
-    assert_eq! (unsafe {
-                & ( * ( 0 as * const max_align_t ) ) .
-                __clang_max_align_nonce2 as * const _ as usize } , 16usize ,
-                concat ! (
-                "Alignment of field: " , stringify ! ( max_align_t ) , "::" ,
-                stringify ! ( __clang_max_align_nonce2 ) ));
-}
-impl Clone for max_align_t {
-    fn clone(&self) -> Self { *self }
-}
 extern "C" {
     pub fn sfMusic_createFromFile(filename: *const ::std::os::raw::c_char)
      -> *mut sfMusic;
