677379d3851be45e901cf4f10c41f29dbe06bf9c
crumblingstatue <radiantstatue@gmail.com>
2020-04-20 15:24:09 +0200

    build: Use runtime env vars for determining target info
    This allows proper cross-compilation

--- ffi/sfml-build/src/lib.rs
+++ ffi/sfml-build/src/lib.rs
@@ -4,8 +4,8 @@ use std::env::var;
 pub fn link_csfml(lib_name: &str) {
     // Figure out the path to libraries within the CSFML base folder
     // based on the operating system
-    let lib_path = if cfg!(target_family = "windows") {
-        if cfg!(target_env = "msvc") {
+    let lib_path = if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
+        if std::env::var("CARGO_CFG_TARGET_ENV").unwrap() == "msvc" {
             "lib/msvc"
         } else {
             "lib/gcc"
@@ -17,7 +17,9 @@ pub fn link_csfml(lib_name: &str) {
     // CSFML_HOME points to the base CSFML directory
     // Let cargo find the CSFML library files there
     if let Ok(csfml_home) = var("CSFML_HOME") {
-        println!("cargo:rustc-link-search=native={}/{}", csfml_home, lib_path);
+        let search_path = format!("{}/{}", csfml_home, lib_path);
+        println!("cargo:warning=CSFML search path is {}", search_path);
+        println!("cargo:rustc-link-search=native={}", search_path);
     }
 
     // Link to the csfml library
