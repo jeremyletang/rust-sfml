e3422bff354620b7bc7a385f7633cb68e46b6cd8
crumblingstatue <radiantstatue@gmail.com>
2020-09-07 10:39:42 +0200

    Fix accidental breakage of SfStr::from_ptr_str

--- src/system/string.rs
+++ src/system/string.rs
@@ -9,7 +9,8 @@ pub struct SfStr(U32CStr);
 
 impl SfStr {
     pub(crate) unsafe fn from_ptr_str<'a>(p: *const u32) -> &'a Self {
-        U32CStr::from_ptr_str(p) as &Self
+        let ptr: *const U32CStr = U32CStr::from_ptr_str(p);
+        &*(ptr as *const Self)
     }
     /// Convert to a UTF-8 `String` from the Rust standard library.
     ///
