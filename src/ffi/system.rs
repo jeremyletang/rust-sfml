faa32e3c9ab0fb1d9fe647ce089e86fd241f80e9
crumblingstatue <radiantstatue@gmail.com>
2022-08-12 14:49:28 +0200

    Put rust-sfml-bindgen generated bindings into separate files
    This makes it easier to auto generate them, without having to
    copy-paste.

--- src/ffi/system.rs
+++ src/ffi/system.rs
@@ -130,52 +130,10 @@ impl Display for sfStdString {
     }
 }
 
-extern "C" {
-    // Clock.cpp
-    pub fn sfClock_new() -> *mut sfClock;
-    pub fn sfClock_delete(clock: *mut sfClock);
-    pub fn sfClock_getElapsedTime(clock: *const sfClock) -> i64;
-    pub fn sfClock_restart(clock: *mut sfClock) -> i64;
-
-    // InputStream.cpp
-    pub fn sfInputStream_new(
-        read: sfInputStreamReadFunc,
-        seek: sfInputStreamSeekFunc,
-        tell: sfInputStreamTellFunc,
-        getSize: sfInputStreamGetSizeFunc,
-        userData: *mut c_void,
-    ) -> *mut sfInputStream;
-    pub fn sfInputStream_destroy(stream: *mut sfInputStream);
-
-    // SfStdString.cpp
-    pub fn sfStdString_getLength(s: *const sfStdString) -> usize;
-    pub fn sfStdString_getData(s: *const sfStdString) -> *const c_char;
-    pub fn sfStdString_destroy(s: *mut sfStdString);
-
-    // SfStdVector.cpp
-    pub fn sfStdStringVector_destroy(vec: *mut sfStdStringVector);
-    pub fn sfStdStringVector_getLength(vec: *const sfStdStringVector) -> usize;
-    pub fn sfStdStringVector_index(
-        vec: *const sfStdStringVector,
-        index: usize,
-    ) -> *const sfStdString;
-    pub fn sfVideoModeVector_getLength(vec: *const sfVideoModeVector) -> usize;
-    pub fn sfVideoModeVector_index(
-        vec: *const sfVideoModeVector,
-        index: usize,
-    ) -> *const sfVideoMode;
-
-    // SfString.cpp
-    pub fn sfString_getLength(string: *const sfString) -> usize;
-    pub fn sfString_getData(string: *const sfString) -> *const u32;
-    pub fn sfString_delete(string: *mut sfString);
-
-    // Sleep.cpp
-    pub fn sfSleep(duration_ms: i64);
-}
-
 type sfInputStreamReadFunc =
     Option<unsafe extern "C" fn(data: *mut c_void, size: i64, userData: *mut c_void) -> i64>;
 type sfInputStreamSeekFunc = Option<unsafe extern "C" fn(pos: i64, user_data: *mut c_void) -> i64>;
 type sfInputStreamTellFunc = Option<unsafe extern "C" fn(userData: *mut c_void) -> i64>;
 type sfInputStreamGetSizeFunc = Option<unsafe extern "C" fn(user_data: *mut c_void) -> i64>;
+
+include!("system_bindgen.rs");
