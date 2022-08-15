3fa6661f8d730bdecafebea4577e2036e64e6286
crumblingstatue <radiantstatue@gmail.com>
2022-08-15 14:16:25 +0200

    CSFML/Graphics: Inline GLSL types

--- CSFML/src/Graphics/Shader.cpp
+++ CSFML/src/Graphics/Shader.cpp
@@ -1,7 +1,71 @@
-#include "Graphics/Glsl.h"
 #include "System/InputStreamStruct.h"
+#include "System/Vector2.h"
+#include "System/Vector3.h"
 #include <SFML/Graphics/Shader.hpp>
 #include <cstddef>
+
+// 2D vectors
+typedef sfVector2f sfGlslVec2;
+typedef sfVector2i sfGlslIvec2;
+
+typedef struct
+{
+    bool x;
+    bool y;
+} sfGlslBvec2;
+
+// 3D vectors
+typedef sfVector3f sfGlslVec3;
+
+typedef struct
+{
+    int x;
+    int y;
+    int z;
+} sfGlslIvec3;
+
+typedef struct
+{
+    bool x;
+    bool y;
+    bool z;
+} sfGlslBvec3;
+
+// 4D vectors
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+    float w;
+} sfGlslVec4;
+
+typedef struct
+{
+    int x;
+    int y;
+    int z;
+    int w;
+} sfGlslIvec4;
+
+typedef struct
+{
+    bool x;
+    bool y;
+    bool z;
+    bool w;
+} sfGlslBvec4;
+
+// matrices
+typedef struct
+{
+    float array[3 * 3];
+} sfGlslMat3;
+
+typedef struct
+{
+    float array[4 * 4];
+} sfGlslMat4;
 
 extern "C" sf::Shader *sfShader_defaultConstruct() {
     return new sf::Shader;
