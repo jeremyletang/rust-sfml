1e2c6f2e80cc4bb86bfd9da4116d49186bc685ff
crumblingstatue <radiantstatue@gmail.com>
2022-08-15 14:00:13 +0200

    CSFML/Audio: Inline SoundStream and SoundRecorder helpers

--- CSFML/src/Audio/SoundRecorder.cpp
+++ CSFML/src/Audio/SoundRecorder.cpp
@@ -1,7 +1,54 @@
-#include "Audio/SoundRecorderStruct.h"
+#include <SFML/Audio/SoundRecorder.hpp>
 #include <cstddef>
 #include <cstdint>
 #include <vector>
+
+typedef bool (*sfSoundRecorderStartCallback)(void *);                            ///< Type of the callback used when starting a capture
+typedef bool (*sfSoundRecorderProcessCallback)(const int16_t *, size_t, void *); ///< Type of the callback used to process audio data
+typedef void (*sfSoundRecorderStopCallback)(void *);                             ///< Type of the callback used when stopping a capture
+
+// Helper class implementing the callback forwarding from
+// C++ to C in sfSoundRecorder
+class sfSoundRecorder : public sf::SoundRecorder {
+  public:
+    sfSoundRecorder(sfSoundRecorderStartCallback onStart,
+                    sfSoundRecorderProcessCallback onProcess,
+                    sfSoundRecorderStopCallback onStop,
+                    void *userData) : myStartCallback(onStart),
+                                      myProcessCallback(onProcess),
+                                      myStopCallback(onStop),
+                                      myUserData(userData) {
+    }
+
+    void setProcessingInterval(int64_t interval) {
+        sf::SoundRecorder::setProcessingInterval(sf::microseconds(interval));
+    }
+
+  private:
+    virtual bool onStart() {
+        if (myStartCallback)
+            return myStartCallback(myUserData);
+        else
+            return true;
+    }
+
+    virtual bool onProcessSamples(const sf::Int16 *samples, std::size_t sampleCount) {
+        if (myProcessCallback)
+            return myProcessCallback(samples, sampleCount, myUserData);
+        else
+            return true;
+    }
+
+    virtual void onStop() {
+        if (myStopCallback)
+            myStopCallback(myUserData);
+    }
+
+    sfSoundRecorderStartCallback myStartCallback;
+    sfSoundRecorderProcessCallback myProcessCallback;
+    sfSoundRecorderStopCallback myStopCallback;
+    void *myUserData;
+};
 
 extern "C" sfSoundRecorder *sfSoundRecorder_create(sfSoundRecorderStartCallback onStart,
                                                    sfSoundRecorderProcessCallback onProcess,
--- CSFML/src/Audio/SoundStream.cpp
+++ CSFML/src/Audio/SoundStream.cpp
@@ -1,6 +1,53 @@
-#include "Audio/SoundStreamStruct.h"
 #include "System/Vector3.h"
+#include <SFML/Audio/SoundStream.hpp>
 #include <cstddef>
+#include <cstdint>
+
+typedef struct
+{
+    int16_t *samples;         ///< Pointer to the audio samples
+    unsigned int sampleCount; ///< Number of samples pointed by Samples
+} sfSoundStreamChunk;
+
+typedef bool (*sfSoundStreamGetDataCallback)(sfSoundStreamChunk *, void *); ///< Type of the callback used to get a sound stream data
+typedef void (*sfSoundStreamSeekCallback)(int64_t, void *);                 ///< Type of the callback used to seek in a sound stream
+
+// Helper class implementing the callback forwarding from
+// C++ to C in sfSoundStream
+class sfSoundStream : public sf::SoundStream {
+  public:
+    sfSoundStream(sfSoundStreamGetDataCallback onGetData,
+                  sfSoundStreamSeekCallback onSeek,
+                  unsigned int channelCount,
+                  unsigned int sampleRate,
+                  void *userData) : myGetDataCallback(onGetData),
+                                    mySeekCallback(onSeek),
+                                    myUserData(userData) {
+        initialize(channelCount, sampleRate);
+    }
+
+  private:
+    virtual bool onGetData(Chunk &data) {
+        sfSoundStreamChunk chunk = {NULL, 0};
+        bool ok = (myGetDataCallback(&chunk, myUserData));
+
+        data.samples = chunk.samples;
+        data.sampleCount = chunk.sampleCount;
+
+        return ok;
+    }
+
+    virtual void onSeek(sf::Time timeOffset) {
+        if (mySeekCallback) {
+            int64_t time = {timeOffset.asMicroseconds()};
+            mySeekCallback(time, myUserData);
+        }
+    }
+
+    sfSoundStreamGetDataCallback myGetDataCallback;
+    sfSoundStreamSeekCallback mySeekCallback;
+    void *myUserData;
+};
 
 extern "C" sfSoundStream *sfSoundStream_create(sfSoundStreamGetDataCallback onGetData,
                                                sfSoundStreamSeekCallback onSeek,
