3c732e8516239d13fdacec7b79ecffcbc34d3707
Shane Whitmire <dogunbound5@gmail.com>
2024-02-18 17:40:45 -0600

    Implement safer pixel data functions
    I believe that it is unnecessary for the user to use unsafe functions
    when it can easily be implemented with safe functionality. This is an
    example of doing it safely with a small abstraction. Rust is all about
    safe code, not unsafe code. Such a simple abstraction should be hidden
    from the user

--- src/window/context.rs
+++ src/window/context.rs
@@ -69,8 +69,8 @@ impl Context {
     /// Get the address of an OpenGL function.
     /// # Arguments
     /// * name - Name of the function to get the address of
-    /// 
-    /// Returns the address of the OpenGL function, 0 on failure 
+    ///
+    /// Returns the address of the OpenGL function, 0 on failure
     pub unsafe fn get_function(name: &CStr) -> *const std::ffi::c_void {
         unsafe { ffi::sfContext_getFunction(name.as_ptr()) }
     }
