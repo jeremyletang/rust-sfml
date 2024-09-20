80d3a76f283684c56834f2c1dc61a81fb8f58182
crumblingstatue <radiantstatue@gmail.com>
2024-09-20 20:36:46 +0200

    Fix weird wording about license in crate root documentation

--- src/lib.rs
+++ src/lib.rs
@@ -29,11 +29,10 @@
 //!
 //! # License
 //!
-//! This software is a binding of the SFML library created by Laurent Gomila, which
-//! is provided under the Zlib/png license.
+//! This software is a binding of the SFML library created by Laurent Gomila,
+//! which is provided under the Zlib/png license.
 //!
-//! This software is provided under the same license than the SFML, the Zlib/png
-//! license.
+//! This software is provided under the same license as SFML, the Zlib/png license.
 //!
 
 #![warn(
