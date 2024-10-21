2851b68b755c07d1d49fa890520ca439bd5f2140
crumblingstatue <radiantstatue@gmail.com>
2024-10-21 21:02:23 +0200

    CSFML: "final"-ize InputStreamHelper

--- CSFML/src/System/InputStreamHelper.hpp
+++ CSFML/src/System/InputStreamHelper.hpp
@@ -9,24 +9,24 @@ typedef int64_t (*sfInputStreamHelperSeekCb)(int64_t position, void *userData);
 typedef int64_t (*sfInputStreamHelperTellCb)(void *userData);
 typedef int64_t (*sfInputStreamHelperGetSizeCb)(void *userData);
 
-struct sfInputStreamHelper : public sf::InputStream {
+struct sfInputStreamHelper final : public sf::InputStream {
     sfInputStreamHelper(sfInputStreamHelperReadCb read,
                         sfInputStreamHelperSeekCb seek,
                         sfInputStreamHelperTellCb tell,
                         sfInputStreamHelperGetSizeCb getSize, void *userData);
-    virtual sf::Int64 read(void *data, sf::Int64 size) {
+    virtual sf::Int64 read(void *data, sf::Int64 size) final {
         return readCb(data, size, userData);
     }
 
-    virtual sf::Int64 seek(sf::Int64 position) {
+    virtual sf::Int64 seek(sf::Int64 position) final {
         return seekCb(position, userData);
     }
 
-    virtual sf::Int64 tell() {
+    virtual sf::Int64 tell() final {
         return tellCb(userData);
     }
 
-    virtual sf::Int64 getSize() {
+    virtual sf::Int64 getSize() final {
         return getSizeCb(userData);
     }
     sfInputStreamHelperReadCb readCb;
