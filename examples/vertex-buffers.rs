65da24012a4179f87306599b1c2973cf92c0a288
crumblingstatue <radiantstatue@gmail.com>
2024-10-21 17:51:48 +0200

    examples/vertex-buffers: Make the example more dynamic (mouse clicks add vertices)

--- examples/vertex-buffers.rs
+++ examples/vertex-buffers.rs
@@ -2,7 +2,7 @@ use sfml::{
     graphics::{
         Color, PrimitiveType, RenderTarget, RenderWindow, Vertex, VertexBuffer, VertexBufferUsage,
     },
-    window::{Event, Style},
+    window::{mouse::Button, Event, Style},
     SfResult,
 };
 
@@ -15,10 +15,7 @@ fn main() -> SfResult<()> {
     )?;
     window.set_vertical_sync_enabled(true);
 
-    let mut vertex_buffer =
-        VertexBuffer::new(PrimitiveType::LINE_STRIP, 6, VertexBufferUsage::STATIC)?;
-
-    let vertices = [
+    let mut vertices = vec![
         Vertex::with_pos_color((20.0, 30.0).into(), Color::GREEN),
         Vertex::with_pos_color((30.0, 30.0).into(), Color::GREEN),
         Vertex::with_pos_color((40.0, 40.0).into(), Color::GREEN),
@@ -26,12 +23,29 @@ fn main() -> SfResult<()> {
         Vertex::with_pos_color((60.0, 60.0).into(), Color::GREEN),
         Vertex::with_pos_color((50.0, 80.0).into(), Color::GREEN),
     ];
+    let mut vertex_buffer = VertexBuffer::new(
+        PrimitiveType::LINE_STRIP,
+        vertices.len(),
+        VertexBufferUsage::DYNAMIC,
+    )?;
     vertex_buffer.update(&vertices, 0)?;
 
     'mainloop: loop {
         while let Some(e) = window.poll_event() {
-            if e == Event::Closed {
-                break 'mainloop;
+            match e {
+                Event::Closed => break 'mainloop,
+                Event::MouseButtonPressed {
+                    button: Button::Left,
+                    x,
+                    y,
+                } => {
+                    vertices.push(Vertex::with_pos_color(
+                        (x as f32, y as f32).into(),
+                        Color::GREEN,
+                    ));
+                    vertex_buffer.update(&vertices, 0)?;
+                }
+                _ => {}
             }
         }
         // Clear the window
