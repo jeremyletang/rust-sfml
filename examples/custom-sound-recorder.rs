168248bc58eddee5667467188991674ba5ca24c4
crumblingstatue <radiantstatue@gmail.com>
2024-10-22 13:45:26 +0200

    Showcase more public API items in custom-sound-recorder example

--- examples/custom-sound-recorder.rs
+++ examples/custom-sound-recorder.rs
@@ -1,5 +1,5 @@
 use {
-    sfml::audio::{SoundRecorder, SoundRecorderDriver},
+    sfml::audio::{capture, SoundRecorder, SoundRecorderDriver},
     std::{error::Error, fs::File, io::Write},
 };
 
@@ -29,9 +29,51 @@ impl FileRecorder {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    assert!(
+        capture::is_available(),
+        "Sorry, audio capture is not supported by your system"
+    );
+    let default_device = capture::default_device();
+    let devices = capture::available_devices();
+    println!("{} recording devices available:", devices.len());
+    for (i, device) in devices.iter().enumerate() {
+        let def_str = if device == &*default_device {
+            " (default)"
+        } else {
+            ""
+        };
+        println!("Device {i}: {device}{def_str}");
+    }
+    print!("Enter device and channel count: ");
+    std::io::stdout().flush()?;
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    let mut tokens = input.trim().split_whitespace();
+    let device_idx = match tokens.next() {
+        Some(tok) => tok.parse::<usize>()?,
+        None => 0,
+    };
+    let channel_count = match tokens.next() {
+        Some(tok) => tok.parse::<u32>()?,
+        None => 1,
+    };
     let mut fr = FileRecorder::create("hello.pcm")?;
     let mut recorder = SoundRecorderDriver::new(&mut fr);
+    match devices.get(device_idx) {
+        Some(dev) => recorder.set_device(dev.to_str()?)?,
+        None => eprintln!("Invalid device index: {device_idx}"),
+    }
+    recorder.set_channel_count(channel_count);
     recorder.start(44_100)?;
+    println!(
+        "Recorder properties:\n\
+               sample rate: {}\n\
+               channel count: {}\n\
+               device: {}",
+        recorder.sample_rate(),
+        recorder.channel_count(),
+        recorder.device()
+    );
     let mut left = 5000;
     while left > 0 {
         std::thread::sleep(std::time::Duration::from_millis(100));
